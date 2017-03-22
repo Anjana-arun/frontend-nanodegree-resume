349f31536fcfeec3f021ff90fb8999042fe041a4
Anjana <anjoo.pilai@gmail.com>
2017-03-22 18:01:56 +0530

    Add Name and role

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,2 +1,4 @@
 $("#main").append("Anjana Arun");
 var awesomeThoughts="I am Anjana and I am AWESOME"
+var formattedName=HTMLheaderName.replace("%data","ANJANA");
+var formattedRole=HTMLheaderRole.replace("%data","STUDENT");
